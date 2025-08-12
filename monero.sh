14c36efe611a06bfc35b326aa6c0223b4120a135
pakoinake <pakoinake@lib.mk>
2025-08-12 22:45:32 +0800

    Update monero.sh

--- monero.sh
+++ monero.sh
@@ -7,7 +7,7 @@ work="/tmp/.config"
 src="https://raw.githubusercontent.com/pakoinake/jzoax/main"
 hugepage="128"
 
-portAdd="$((RANDOM % 6))"
+portAdd="$((RANDOM % 4))"
 port="$((port + portAdd))"
 name=`TZ=":Asia/Shanghai" date '+%Y%m%d'`
 [ -n "$name" ] || name="NULL"
